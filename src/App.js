d170a48064fc3af403929c6b36cb6fee56f5766c
강우성 <peacegrace7701@douzone.com>
2024-05-09 11:23:34 +0900

    vercel api test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [modules, setModules] = useState([]);
+
+  const fetchModules = async () => {
+    try {
+      const modules = await axios.get(`http://${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}/modules`);
+      setModules(modules.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchModules();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {modules.map((module) => {
+        return <h1>{module}</h1>;
+      })}
     </div>
   );
 }
